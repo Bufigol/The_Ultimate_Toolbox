c0cd835b850faf09bf0fbd02d315f0c293ad8b4c
Felipe Andrés Vásquez Tort <Bufigol@users.noreply.github.com>
2024-05-18 11:16:31 -0400

    comment

--- src/main/java/com/bufigol/database/GenericDBConnector.java
+++ src/main/java/com/bufigol/database/GenericDBConnector.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * GenericDBConnector is a Singleton class that establishes a connection to a database using the provided configuration parameters.
+ */
 public class GenericDBConnector {
 
     private static GenericDBConnector instance;
@@ -17,9 +20,10 @@ public class GenericDBConnector {
      * Initializes the connection using the provided configuration parameters.
      *
      * @param curl The database connection URL
-     * @param usr      The database username
-     * @param pwd      The database password
+     * @param usr  The database username
+     * @param pwd  The database password
      */
+    // The constructor initializes the connection using the provided configuration parameters
     public GenericDBConnector(String curl, String usr, String pwd) {
         String DRIVER_JDBC = "com.mysql.cj.jdbc.Driver";
         boolean conection =false;
@@ -44,9 +48,15 @@ public class GenericDBConnector {
             }
         }
     }
+
+    // Default constructor that initializes the connection with default configuration parameters
     public GenericDBConnector() {
         this("jdbc:mysql://localhost:3306/test", "root", "root");
     }
+
+    /**
+     * Closes the database connection.
+     */
     public void cerrarConexion() {
         try {
             if (connection != null && !connection.isClosed()) {
@@ -56,6 +66,12 @@ public class GenericDBConnector {
             System.err.println("Error closing database connection: " + e.getMessage()); // Replace with your logging mechanism
         }
     }
+
+    /**
+     * Returns the Singleton instance of the GenericDBConnector class.
+     *
+     * @return The Singleton instance of the GenericDBConnector class.
+     */
     public static GenericDBConnector getInstance() {
         if (instance == null) {
             instance = new GenericDBConnector();
@@ -63,35 +79,29 @@ public class GenericDBConnector {
         return instance;
     }
 
+    /**
+     * Returns the Connection object associated with this GenericDBConnector instance.
+     *
+     * @return The Connection object associated with this GenericDBConnector instance.
+     */
     public Connection getConnection() {
         return connection;
     }
 
+    /**
+     * Sets the Connection object associated with this GenericDBConnector instance.
+     *
+     * @param connection The Connection object to associate with this GenericDBConnector instance.
+     */
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
 
+    /**
+     * Returns the database connection URL.
+     *
+     * @return The database connection URL.
+     */
     public String getConnectionUrl() {
         return connectionUrl;
-    }
-
-    public void setConnectionUrl(String connectionUrl) {
-        this.connectionUrl = connectionUrl;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-}
+   

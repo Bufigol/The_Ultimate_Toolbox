c7753071e95cbd0086042c1d3c2860bb561bf66f
Felipe Andrés Vásquez Tort <Bufigol@users.noreply.github.com>
2024-05-18 11:16:29 -0400

    comment

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,13 @@
-import com.bufigol.database.GenericDBConnector;
+import com.bufigol.database.GenericDBConnector; // Importing the GenericDBConnector class from the com.bufigol.database package
 
 public class test {
+    
     public static void main(String[] args) {
-        GenericDBConnector db = new GenericDBConnector("jdbc:mysql://localhost:3306/vinosproject", "root", "admin");
+        // Create a new instance of the GenericDBConnector class, which is used to connect to a MySQL database
+        GenericDBConnector db = new GenericDBConnector(
+            "jdbc:mysql://localhost:3306/vinosproject", // The JDBC URL for the MySQL database, including the hostname, port number, database name
+            "root", // The username used to connect to the database
+            "admin" // The password used to connect to the database
+        );
     }
 }

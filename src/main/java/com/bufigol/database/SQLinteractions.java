4022044877971aa717bcc474a8cc35d1a4490d23
Felipe Andrés Vásquez Tort <f.vasquez.tort@gmail.com>
2024-05-20 16:59:50 -0400

    new dependencies and fixed minor problems

--- src/main/java/com/bufigol/database/SQLinteractions.java
+++ src/main/java/com/bufigol/database/SQLinteractions.java
@@ -1,10 +1,14 @@
 package com.bufigol.database;
 
-import com.bufigol.textToolbox.BasicTextToolBox;
 
 import java.sql.*;
 import java.util.ArrayList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/**
+ *
+ */
 public class SQLinteractions {
     /**
      * Inserts a single record into the specified table.
@@ -14,9 +18,8 @@ public class SQLinteractions {
      * @param columns    an array of column names
      * @param values     an array of values corresponding to the columns
      * @return true if the record was inserted successfully, false otherwise
-     * @throws SQLException if a database access error occurs
      */
-    public static boolean insertIntoTableOneRecord(Connection connection, String table, String[] columns, String[] values) throws SQLException {
+    public static boolean insertIntoTableOneRecord(Connection connection, String table, String[] columns, String[] values){
         if (columns.length != values.length) {
             throw new IllegalArgumentException("The number of columns and values must be the same.");
         }
@@ -52,10 +55,12 @@ public class SQLinteractions {
 
             // Retorna true si al menos una fila fue insertada, de lo contrario retorna false
             return filasInsertadas > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static boolean insertIntoTableOneRecord(Connection connection, String table, String[] columns, String[] values, String[] types) throws SQLException {
+    public static boolean insertIntoTableOneRecord(Connection connection, String table, String[] columns, String[] values, String[] types) {
         StringBuilder queryBuilder = new StringBuilder("INSERT INTO ");
         queryBuilder.append(table).append(" (");
         // Construye la lista de columnas en la consulta SQL
@@ -102,18 +107,23 @@ public class SQLinteractions {
 
             // Retorna true si al menos una fila fue insertada, de lo contrario retorna false
             return filasInsertadas > 0;
+        } catch (SQLException e) {
+            Logger logger = LogManager.getLogger(SQLinteractions.class);
+            logger.error("An error occurred while searching", e);
+            throw new RuntimeException(e);
         }
     }
 
     /**
-     * @param connection
-     * @param table
-     * @param columns
-     * @param values
-     * @return
-     * @throws SQLException
+     * Inserts multiple records into the specified table.
+     *
+     * @param connection the database connection
+     * @param table      the name of the table
+     * @param columns    an array of column names
+     * @param values     a 2-dimensional array of values corresponding to the columns
+     * @return the number of rows inserted, or 0 if no rows were inserted
      */
-    public static int insertIntoTableMulipleRecords(Connection connection, String table, String[] columns, String[][] values) throws SQLException {
+    public static int insertIntoTableMulipleRecords(Connection connection, String table, String[] columns, String[][] values) {
         boolean out = true;
         int i = 0;
         while (i < values.length && out) {
@@ -123,8 +133,17 @@ public class SQLinteractions {
         return i;
     }
 
-    public static int insertIntoTableMulipleRecords(Connection connection, String table, String[] columns, String[][] values, String[] types) throws SQLException {
-        Boolean out = true;
+    /**
+     * Inserts multiple records into the specified table.
+     *
+     * @param connection the database connection
+     * @param table      the name of the table
+     * @param columns    an array of column names
+     * @param values     a 2-dimensional array of values corresponding to the columns
+     * @return the number of rows inserted, or 0 if no rows were inserted
+     */
+    public static int insertIntoTableMulipleRecords(Connection connection, String table, String[] columns, String[][] values, String[] types) {
+        boolean out = true;
         int i = 0;
         while (i < values.length && out) {
             out = insertIntoTableOneRecord(connection, table, columns, values[i], types);
@@ -139,9 +158,8 @@ public class SQLinteractions {
      * @param connection the database connection
      * @param table      the name of the table
      * @return the number of rows in the table, or 0 if the table is empty
-     * @throws SQLException if a database access error occurs
      */
-    public static int countRows(Connection connection, String table) throws SQLException {
+    public static int countRows(Connection connection, String table) {
         String sql = "SELECT COUNT(*) FROM " + table;
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             try (ResultSet resultSet = statement.executeQuery()) {
@@ -151,6 +169,10 @@ public class SQLinteractions {
                     return 0;
                 }
             }
+        } catch (SQLException e) {
+            Logger logger = LogManager.getLogger(SQLinteractions.class);
+            logger.error("An error occurred while searching", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -161,13 +183,14 @@ public class SQLinteractions {
      * @param table      the name of the table
      * @param id         the ID of the row to be deleted
      * @return true if the row was deleted successfully, false otherwise
-     * @throws SQLException if a database access error occurs
      */
-    public static boolean deleteRowByID(Connection connection, String table, int id) throws SQLException {
+    public static boolean deleteRowByID(Connection connection, String table, int id)  {
         String sql = "DELETE FROM " + table + " WHERE id = ?";
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
             return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -178,9 +201,8 @@ public class SQLinteractions {
      * @param table      the name of the table
      * @param id         an array of IDs of the rows to be deleted
      * @return true if all rows were deleted successfully, false otherwise
-     * @throws SQLException if a database access error occurs
      */
-    public static boolean deleteRowByID(Connection connection, String table, int[] id) throws SQLException {
+    public static boolean deleteRowByID(Connection connection, String table, int[] id)  {
         int count = 0;
         boolean out = true;
         while (count < id.length && out) {
@@ -189,20 +211,53 @@ public class SQLinteractions {
 
         return out;
     }
-
-    public static boolean updateById(Connection connection, String table, String idColumnName, int id, String[] columns, String[] values, String[] types) throws SQLException {
-        boolean out = false;
-        String sql = "UPDATE " + table + " SET ";
+    /**
+     * Updates a record in the specified table by ID.
+     *
+     * @param connection    the database connection
+     * @param table         the name of the table
+     * @param idColumnName  the name of the ID column
+     * @param id            the ID of the record to update
+     * @param columns       an array of column names to update
+     * @param values        an array of values corresponding to the columns
+     * @param types         an array of data types corresponding to the columns
+     * @return true if the record was updated successfully, false otherwise
+     * @throws RuntimeException if an SQL exception occurs during the update
+     */
+    public static boolean updateById(Connection connection, String table, String idColumnName, int id, String[] columns, String[] values, String[] types) {
+        boolean out;
+        StringBuilder sql = new StringBuilder("UPDATE " + table + " SET ");
         for (int i = 0; i < columns.length; i++) {
-            sql += columns[i] + " = " + values[i];
+            sql.append(columns[i]).append(" = ?");
             if (i < columns.length - 1) {
-                sql += ", ";
+                sql.append(", ");
             }
         }
-        sql += " WHERE " + idColumnName + " = " + id;
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        sql.append(" WHERE ").append(idColumnName).append(" = ?").append(id);
+        try (PreparedStatement statement = connection.prepareStatement(sql.toString())) {
+            for(int i=0;i<types.length;i++){
+                switch (types[i]){
+                    case "INT":
+                        statement.setInt(i+1, Integer.parseInt(values[i]));
+                        break;
+                    case "FLOAT":
+                        statement.setFloat(i+1, Float.parseFloat(values[i]));
+                        break;
+                    case "DOUBLE":
+                        statement.setDouble(i+1, Double.parseDouble(values[i]));
+                        break;
+                    default:
+                        statement.setString(i+1, values[i]);
+                        break;
+                }
+            }
+            statement.setInt(columns.length + 1, id);
             out = statement.executeUpdate() > 0;
             return out;
+        } catch (SQLException e) {
+            Logger logger = LogManager.getLogger(SQLinteractions.class);
+            logger.error("An error occurred while searching", e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -232,17 +287,17 @@ public class SQLinteractions {
 
     public static ArrayList<String[]> searchByMultipleFieldAND(Connection connection, String table, String[] field, String[] values,String[] types) {
         ArrayList<String[]> out = new ArrayList<>();
-        String sql = "SELECT * FROM " + table + " WHERE ";
+        StringBuilder sql = new StringBuilder("SELECT * FROM " + table + " WHERE ");
         if (field.length != values.length) {
             throw new IllegalArgumentException("The number of fields and values must be the same.");
         }
         for (int i = 0; i < field.length; i++) {
-            sql += field[i] + " = ?" ;
+            sql.append(field[i]).append(" = ?");
             if (i < field.length - 1) {
-                sql += " AND ";
+                sql.append(" AND ");
             }
         }
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+        try (PreparedStatement pstmt = connection.prepareStatement(sql.toString())) {
             for (int i = 0; i < values.length; i++) {
                 String type = types[i];
                 switch (type) {
@@ -303,17 +358,32 @@ public class SQLinteractions {
 
     }
 
-    public static ArrayList<String[]> searchByMultipleFieldOR(Connection connection, String table, String[] field, String[] values) throws SQLException {
+    public static ArrayList<String[]> searchByMultipleFieldOR(Connection connection, String table, String[] field, String[] values)  {
         ArrayList<String[]> out = new ArrayList<>();
-        String sql = "SELECT * FROM " + table + " WHERE ";
+        StringBuilder sql = new StringBuilder("SELECT * FROM " + table + " WHERE ");
         if (field.length != values.length) {
             throw new IllegalArgumentException("The number of fields and values must be the same.");
         }
         for (int i = 0; i < field.length; i++) {
-            sql += field[i] + " = " + values[i];
+            sql.append(field[i]).append(" = ").append(values[i]);
             if (i < field.length - 1) {
-                sql += " OR ";
+                sql.append(" OR ");
             }
+        }
+
+        try {
+            PreparedStatement pstmt= connection.prepareStatement(sql.toString());
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int columnCount = rs.getMetaData().getColumnCount();
+                String[] row = new String[columnCount];
+                for (int i = 0; i < columnCount; i++) {
+                    row[i] = rs.getString(i + 1);
+                }
+                out.add(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return out;
     }
@@ -326,10 +396,9 @@ public class SQLinteractions {
      * @param columnas    an array of column names for the new table
      * @param tipos       an array of column types for the new table, corresponding to the columnas array
      * @return true if the table was created successfully, false otherwise
-     * @throws SQLException             if a database access error occurs
      * @throws IllegalArgumentException if the number of columnas and tipos arrays are not equal
      */
-    public static boolean createTable(Connection connection, String nombreTabla, String[] columnas, String[] tipos) throws SQLException {
+    public static boolean createTable(Connection connection, String nombreTabla, String[] columnas, String[] tipos)  {
         if (columnas.length != tipos.length) {
             throw new IllegalArgumentException("El número de columnas y tipos debe ser el mismo.");
         }
@@ -353,6 +422,8 @@ public class SQLinteractions {
             // Ejecutar la consulta para crear la tabla
             stmt.executeUpdate(query);
             return true; // Devolver true si la tabla se creó con éxito
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -389,7 +460,8 @@ public class SQLinteractions {
             statement.close();
             return out;
         } catch (SQLException e) {
-            e.printStackTrace();
+            Logger logger = LogManager.getLogger(SQLinteractions.class);
+            logger.error("An error occurred while searching", e);
             return null;
         }
     }
